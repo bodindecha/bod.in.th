ece630b31e50e362252d2d9b521c755466e5663b
tannce <t@tc.in.th>
2021-07-17 00:19:11 +0700

    change db settings

--- resource/appwork/db_connect.php
+++ resource/appwork/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new mysqli("localhost", "", "", "url_short");
+	$db = new mysqli("localhost", "link_main", "8yRDdTpyzS", "link_main");
 	$db -> set_charset("utf8");
 	// if ($connection -> connect_errno) { die("Connection failed: ".$connection -> connect_error); }
 	// $tcl_db_con -> real_escape_string($ipt_data);
