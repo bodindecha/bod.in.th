905bda8dc5f6da13c5d62ccf286f4c83124ea951
TianTcl <49802993+TianTcl@users.noreply.github.com>
2023-03-20 11:39:44 +0700

    Hide database auth

--- resource/appwork/db_connect.php
+++ resource/appwork/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new mysqli("localhost", "link_main", "8yRDdTpyzS", "link_main");
+	$db = new mysqli("localhost", "DB User", "password", "DB name");
 	$db -> set_charset("utf8");
 	// if ($connection -> connect_errno) { die("Connection failed: ".$connection -> connect_error); }
 	// $tcl_db_con -> real_escape_string($ipt_data);
